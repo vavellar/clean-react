26aff368819ab17719683e45c8f49e1f0c477324
Victor Avellar <victorcandeias@wine.com.br>
2022-04-11 11:00:15 -0300

    fix: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/usecases/authentication/remove-authentication.spec.ts
+++ src/data/usecases/authentication/remove-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authetication'
 import faker from 'faker'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
@@ -18,7 +19,14 @@ describe('RemoteAuthentication', () => {
   test('should call HttpPostClient with correct URL ', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('should call HttpPostClient with correct body ', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const authenticationParams = mockAuthentication()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AuthenticationParams } from 'domain/usecases/authentication'
+import faker from 'faker'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
