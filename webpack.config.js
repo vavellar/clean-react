c34d16f7d298d78516aa76afcb7f8f2ce964ac77
vavellar <victorcandeias@wine.com.br>
2022-04-20 09:15:19 -0300

    feat: add app router

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
         }]
     },
     devServer: {
+        historyApiFallback: true,
         static: {
             directory: path.resolve(__dirname, 'public'),
         },
