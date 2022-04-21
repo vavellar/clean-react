9520db3ff22a35370006ef9e272578a6ce86481e
vavellar <victorcandeias@wine.com.br>
2022-04-21 17:38:43 -0300

    feat: ensure Login start with inputs with required state

--- src/presentation/components/Input/Input.tsx
+++ src/presentation/components/Input/Input.tsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './Input-styles.scss'
+import FormContext from '@/presentation/contexts/form/formContext'
 
 type InputProps ={
   inputType: string
   inputName: string
   placeHolder: string
+  testID: string
 }
 
-const Input: React.FC<InputProps> = ({ inputType, inputName, placeHolder }) => {
+const Input: React.FC<InputProps> = ({
+  inputType,
+  inputName,
+  placeHolder,
+  testID
+}) => {
+  const { emailError, passwordError} = useContext(FormContext)
 
-  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void=> {
-    event.target.readOnly =false 
+  const selectCorrectTextError = (): string => {
+    const options = {
+      'email': emailError,
+      'password': passwordError
+    }
+    return options[inputName]
   }
+
+  const getStatus = (): string => {
+    return '🔴'
+  }
+
+  const getTitle = (): string => {
+    return selectCorrectTextError()
+  }
+
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrap}>
       <input
@@ -19,10 +44,11 @@ const Input: React.FC<InputProps> = ({ inputType, inputName, placeHolder }) => {
         onFocus={enableInput}
         type={inputType}
         name={inputName}
-        id=""
         placeholder={placeHolder}
       />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={testID} title={getTitle()} className={Styles.status}>
+        {getStatus()}
+      </span>
     </div>
   )
 }
--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -3,15 +3,13 @@ import Styles from './Login-styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import FormContext from '@/presentation/contexts/form/formContext'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
 
 const Login: React.FC = () => {
-  const [ state ] = useState < StateProps>({
+  const [ state ] = useState({
     isLoading: false,
-    errorMessage: ''
+    errorMessage: '',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório'
   })
     return (
       <div className={Styles.login}>
@@ -20,16 +18,23 @@ const Login: React.FC = () => {
           <form action="" className={Styles.form}>
             <h2>Login</h2>
             <Input
+              testID="email-status"
               inputType="email"
               inputName="email"
               placeHolder="Digite seu e-mail"
             />
             <Input
+              testID="password-status"
               inputType="password"
               inputName="password"
               placeHolder="Digite sua senha"
             />
-            <button data-testid="submit" type="submit" className={Styles.submit} disabled>
+            <button
+              data-testid="submit"
+              type="submit"
+              className={Styles.submit}
+              disabled
+            >
               Entrar
             </button>
             <span className={Styles.link}>Criar conta</span>
