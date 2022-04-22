21f5cc015e700d3f90675bf87c31fae3fa3d4a36
vavellar <victorcandeias@wine.com.br>
2022-04-21 23:35:19 -0300

    refactor: change Login tests to use faker

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './Login'
 import { ValidationSpy } from '@/presentation/test'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RenderResult
@@ -37,17 +38,19 @@ describe('Login component', () => {
     it('Should call Validation email with correct value', () => {
         const { sut, validationSpy } = makeSut()
         const emailInput = sut.getByTestId('email')
-        fireEvent.input(emailInput, {target: { value: 'any_email'}})
+        const email = faker.internet.email()
+        fireEvent.input(emailInput, { target: { value: email } })
         expect(validationSpy.fieldName).toBe('email')
-        expect(validationSpy.fieldValue).toBe('any_email')
+        expect(validationSpy.fieldValue).toBe(email)
     })
 
     it('Should call Validation password with correct value', () => {
         const { sut, validationSpy } = makeSut()
         const passwordInput = sut.getByTestId('password')
-        fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+        const password = faker.internet.password()
+        fireEvent.input(passwordInput, { target: { value: password } })
         expect(validationSpy.fieldName).toBe('password')
-        expect(validationSpy.fieldValue).toBe('any_password')
+        expect(validationSpy.fieldValue).toBe(password)
 
     })
 })
