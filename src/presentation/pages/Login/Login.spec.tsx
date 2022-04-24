3bbddbcd3ff428ed8a4ed17e590086273de019b4
vavellar <victorcandeias@wine.com.br>
2022-04-24 12:09:24 -0300

    test: ensure Login presente valid state if email is valid

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -68,4 +68,16 @@ describe('Login component', () => {
        expect(passwordStatus.title).toBe('Tudo certo')
        expect(passwordStatus.textContent).toBe('🟢')
      })
+
+     it('Should show valid email state if Validation succeeds', () => {
+       const { sut, validationStub } = makeSut()
+       validationStub.errorMessage = null
+       const emailInput = sut.getByTestId('email')
+       fireEvent.input(emailInput, {
+         target: { value: faker.internet.email() }
+       })
+       const emailStatus = sut.getByTestId('email-status')
+       expect(emailStatus.title).toBe('Tudo certo')
+       expect(emailStatus.textContent).toBe('🟢')
+     })
 })
