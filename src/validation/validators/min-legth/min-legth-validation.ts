28982730f8d29a119f02df0ddd85669aa408a35f
vavellar <victorcandeias@wine.com.br>
2022-04-28 23:28:08 -0300

    feat: ensure MinLengthValidation return falsy if length is bigger than 5

--- src/validation/validators/min-legth/min-legth-validation.ts
+++ src/validation/validators/min-legth/min-legth-validation.ts
@@ -4,6 +4,6 @@ import { FieldValidation } from "@/validation/protocols/field-validation"
 export class MinLegthValidation implements FieldValidation {
   constructor(readonly field: string, private readonly minLength: number) {}
   validate(value: string): Error {
-    return new MinLegthValidationError()
+    return value.length >= this.minLength ? null : new MinLegthValidationError()
   }
 }
