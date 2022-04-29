6a52d6f5ccee1eadf12cc1459a3b87ccf824cf3a
vavellar <victorcandeias@wine.com.br>
2022-04-28 23:34:09 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/min-legth/min-legth-validation.spec.ts
+++ src/validation/validators/min-legth/min-legth-validation.spec.ts
@@ -1,16 +1,19 @@
 import { MinLegthValidationError } from "@/validation/errors"
 import { MinLegthValidation } from "./min-legth-validation"
+import faker from 'faker'
+
+const makeSut = (): MinLegthValidation => new MinLegthValidation(faker.database.column(), 5)
 
 describe('MinLengthValidation', () => {
     test('Should return error if length is less than 5', () => {
-        const sut = new MinLegthValidation('email', 5)
-        const error = sut.validate('123')
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(4))
         expect(error).toEqual(new MinLegthValidationError())
     });
 
     test('Should return falsy if length is bigger than 5', () => {
-      const sut = new MinLegthValidation('email', 5)
-      const error = sut.validate('12345')
+      const sut = makeSut()
+      const error = sut.validate(faker.random.alphaNumeric(5))
       expect(error).toBeFalsy()
     })
 });
