267eece507e64263822c2269d6f14b363e28e5aa
vavellar <victorcandeias@wine.com.br>
2022-04-29 23:02:27 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './min-legth/min-legth-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
