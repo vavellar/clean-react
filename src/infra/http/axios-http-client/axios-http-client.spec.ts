0a31c8068acbfbcc7eb02eb366f6a25742d33fb7
vavellar <victorcandeias@wine.com.br>
2022-05-01 16:37:25 -0300

    fix: change AxiosHttp client to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 import { AxiosHttpClient } from './axios-http-client'
-import { mockAxios } from '@/infra/test'
+import { mockAxios, mockHttpResponse } from '@/infra/test'
 import axios from 'axios'
 import { mockPostRequest } from '@/data/test'
 
@@ -29,4 +29,13 @@ describe('AxiosHttpClient', () => {
         const promise = sut.post(mockPostRequest())
             expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
     })
+
+     test('Should return the correct statusCode and body on failure', () => {
+       const { sut, mockedAxios } = makeSut()
+       mockedAxios.post.mockRejectedValueOnce({
+         response: mockHttpResponse()
+       })
+       const promise = sut.post(mockPostRequest())
+       expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+     })
 });
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,13 +1,18 @@
 import { HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http"
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 // design pattern adapter, adpting two differents interfaces, our system is not axios dependent
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpPostReponse = await axios.post(params.url, params.body)
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (error) {
+      httpResponse = error.response
+    }
     return {
-        statusCode: httpPostReponse.status,
-        body: httpPostReponse.data
+      statusCode: httpResponse.status,
+      body: httpResponse.data
     }
   }
 }
