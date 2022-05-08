178a641c386503e3c8a59c1e3dcff47cdbb6ff7b
vavellar <victorcandeias@wine.com.br>
2022-05-08 16:45:33 -0300

    feat: ensure CompareFieldsValidation returns falsy if validation succeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -4,6 +4,6 @@ import { FieldValidation } from "@/validation/protocols/field-validation"
 export class CompareFieldsValidation implements FieldValidation {
   constructor(readonly field, private readonly valueToCompare: string) {}
   validate(value: string): Error {
-    return new InvalidFieldError(this.field)
+    return value !== this.valueToCompare ? new InvalidFieldError(this.field) : null
   }
 }
