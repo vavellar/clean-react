1bde119ccb170dd4840e46a85a0b01cd0635a9de
vavellar <victorcandeias@wine.com.br>
2022-05-09 07:54:02 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/Signup/Signup.spec.tsx
+++ src/presentation/pages/Signup/Signup.spec.tsx
@@ -38,7 +38,7 @@ describe('SignUp component', () => {
     Helper.testChildCount(sut, 'error-wrap', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
     Helper.testStatusForField(sut, 'name', validationError)
-    Helper.testStatusForField(sut, 'email', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'email', validationError)
     Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
   })
@@ -48,5 +48,12 @@ describe('SignUp component', () => {
      const { sut } = makeSut({ validationError })
      Helper.populateField(sut, 'name')
      Helper.testStatusForField(sut, 'name', validationError)
+   })
+
+   it('Should show email error if Validation fails', () => {
+     const validationError = faker.random.words()
+     const { sut } = makeSut({ validationError })
+     Helper.populateField(sut, 'email')
+     Helper.testStatusForField(sut, 'email', validationError)
    })
 })
