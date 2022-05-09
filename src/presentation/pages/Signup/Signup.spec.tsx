25496d6fec5aa8714ed9f6f730bcfbd1295ce412
vavellar <victorcandeias@wine.com.br>
2022-05-09 08:01:51 -0300

    test: ensure SignUp change email status to success if validation succeds

--- src/presentation/pages/Signup/Signup.spec.tsx
+++ src/presentation/pages/Signup/Signup.spec.tsx
@@ -76,4 +76,10 @@ describe('SignUp component', () => {
      Helper.populateField(sut, 'name')
      Helper.testStatusForField(sut, 'name')
    })
+
+   it('Should show valid email state if Validation succeeds', () => {
+     const { sut } = makeSut()
+     Helper.populateField(sut, 'email')
+     Helper.testStatusForField(sut, 'email')
+   })
 })
