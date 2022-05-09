4e548ad7f376c420a4309437270148f364b1c984
vavellar <victorcandeias@wine.com.br>
2022-05-09 07:58:43 -0300

    test: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/Signup/Signup.spec.tsx
+++ src/presentation/pages/Signup/Signup.spec.tsx
@@ -40,7 +40,7 @@ describe('SignUp component', () => {
     Helper.testStatusForField(sut, 'name', validationError)
     Helper.testStatusForField(sut, 'email', validationError)
     Helper.testStatusForField(sut, 'password', validationError)
-    Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
 
    it('Should show name error if Validation fails', () => {
@@ -62,5 +62,12 @@ describe('SignUp component', () => {
      const { sut } = makeSut({ validationError })
      Helper.populateField(sut, 'password')
      Helper.testStatusForField(sut, 'password', validationError)
+   })
+
+   it('Should show passwordConfirmation error if Validation fails', () => {
+     const validationError = faker.random.words()
+     const { sut } = makeSut({ validationError })
+     Helper.populateField(sut, 'passwordConfirmation')
+     Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
    })
 })
