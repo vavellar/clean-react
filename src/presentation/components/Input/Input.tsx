6b72b7f0237b70733cc75924dd6a5348e8e5d266
vavellar <victorcandeias@wine.com.br>
2022-05-25 09:00:10 -0300

    refactor: test coverage

--- src/presentation/components/Input/Input.tsx
+++ src/presentation/components/Input/Input.tsx
@@ -15,31 +15,10 @@ const Input: React.FC<InputProps> = ({
   inputType,
   inputName,
   placeHolder,
-  testID
 }) => {
   const { state, setState } = useContext(FormContext)
   const inputRef = useRef<HTMLInputElement>()
-  const { emailError, passwordError, nameError, passwordConfirmationError } = state
   const error = state[`${inputName}Error`]
-
-  const selectCorrectTextError = (): string => {
-    const options = {
-      'email': emailError,
-      'password': passwordError,
-      'name': nameError,
-      'passwordConfirmation': passwordConfirmationError
-    }
-    return options[inputName]
-  }
-
-
-  // const getStatus = (): string => {
-  //   return state[`${inputName}Error`] ? '🔴' : '🟢'
-  // }
-
-  // const getTitle = (): string => {
-  //   return state[`${inputName}Error`] ? selectCorrectTextError() : 'Tudo certo'
-  // }
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
