84e76243ae9b57e310a736c247fb354f488080df
vavellar <victorcandeias@wine.com.br>
2022-05-24 21:28:05 -0300

    refactor: move login-mocks to support folder

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,6 +1,6 @@
 import faker from 'faker'
 import * as FormHelper from '../support/form-helper'
-import * as Http from './login-mocks'
+import * as Http from '../support/login-mocks'
 
 describe('Login - Component', () => {
   beforeEach(() => {
