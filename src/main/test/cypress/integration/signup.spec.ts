902c18c67fa26803a3e211fac29bcd72d320ed75
vavellar <victorcandeias@wine.com.br>
2022-05-25 08:04:48 -0300

    test: ensure Signup loads with correct state

---
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -0,0 +1,16 @@
+import faker from 'faker'
+import * as FormHelper from '../support/form-helper'
+
+describe('Signup - Component', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('Should load with correct initial state', () => {
+    FormHelper.testInputStatus('name', 'Campo obrigatório')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+    FormHelper.testInputStatus('password', 'Campo obrigatório')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+  })
+})
