ad2239caa5015088351c4fd5d5d52f2fe9657925
vavellar <victorcandeias@wine.com.br>
2022-06-19 20:10:15 -0300

    refactor: set storage async method

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -3,7 +3,8 @@ import { SetStorage } from "../protocols/cache/set-storage"
 export class SetStorageMock implements SetStorage {
   key: string
   value: any
-  async set(key: string, value: any): Promise<void> {
+  
+  set(key: string, value: any): void {
     this.key = key
     this.value = value
   }
--- src/data/usecases/update-current-account/local-update-current-account.spec.ts
+++ src/data/usecases/update-current-account/local-update-current-account.spec.ts
@@ -31,7 +31,7 @@ describe('LocalUpdateCurrentAccount', () => {
 
     test('Should throw if SetStorage throws', async () => {
       const { sut, setStorageMock } = makeSut()
-      jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
+      jest.spyOn(setStorageMock, 'set').mockImplementationOnce(() => { throw new Error()})
       const promise = sut.save({
         accessToken: faker.datatype.uuid(),
         name: faker.name.findName()
